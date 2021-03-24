2647d433bdc7c81296cf816be559e5f3a8a49098
Sobir <sobirsobirov850@gmail.com>
2021-03-24 21:42:18 +0500

    добавлены типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,28 @@
+package types
+
+//Money представляет собой денежную сумму в минимальных единицах (центы, копейки, дирамы и т.д)
+type Money int64
+
+//Currency представляет код валюты
+type Currency string
+
+//Код валюты
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+//PAN представляет номер карты
+type PAN string
+
+//Card представляет информацию о платежной карте
+type Card struct {
+	ID       int
+	PAN      PAN
+	Balance  Money
+	Currency Currency
+	Color    string
+	Name     string
+	Active   bool
+}
